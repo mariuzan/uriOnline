3cf8e53b2021c3973b6d81c2454571c1a1e4b845
mariuzan <54643449+mariuzan@users.noreply.github.com>
2020-03-17 12:45:04 -0300

    Exercicios Uri

--- src/urionline/iniciante/Exercicio1008.java
+++ src/urionline/iniciante/Exercicio1008.java
@@ -5,10 +5,33 @@
  */
 package urionline.iniciante;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 /**
  *
  * @author DEVELOPER
  */
 public class Exercicio1008 {
+    public static void main(String[]args){
     
+       int idFunc,valorHora;
+       double hora, calculo;
+       
+       Scanner entrada= new Scanner(System.in);
+       DecimalFormat formatador= new DecimalFormat("0.00");
+       
+       idFunc=entrada.nextInt();
+       
+       valorHora= entrada.nextInt();
+       
+       hora= entrada.nextDouble();
+       
+       calculo=valorHora*hora;
+       
+        System.out.println("NUMBER = "+idFunc);
+        
+        System.out.println("SALARY = U$ "+formatador.format(calculo));
+       
+    }
 }
